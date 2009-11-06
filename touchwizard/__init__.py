b0828de16f638474565a07a18941621e05d4dedf
Damien Boucard <damien.boucard@ubicast.eu>
2009-11-06 20:22:55 +0100

    Adding Canvas class ; Adding some constants into __init__.py ;

--- touchwizard/__init__.py
+++ touchwizard/__init__.py
@@ -1,1 +1,8 @@
 from version import VERSION as __revision__
+from canvas import Canvas, quick_launch
+#from page import Page
+#from icon import Icon
+
+page_path = None
+canvas_width = 800
+canvas_height = 480
---
+++ touchwizard/canvas.py
@@ -0,0 +1,93 @@
+import clutter
+import gobject
+
+class Canvas(clutter.Actor, clutter.Container):
+    __gtype_name__ = 'Canvas'
+    infobar_height = 23
+    iconbar_height = 120
+    
+    def __init__(self, first_page):
+        clutter.Actor.__init__(self)
+        self.first_page = first_page
+        
+        self.infobar = clutter.Rectangle()
+        self.infobar.set_color(clutter.color_from_string('LightGray'))
+        
+        self.iconbar = clutter.Rectangle()
+        self.iconbar.set_color(clutter.color_from_string('LightGray'))
+        
+        self.available_pages = self.lookup_pages()
+        self.current_page = None
+    
+    def lookup_pages(self):
+        pass
+    
+    def do_get_preferred_width(self, for_height):
+        import touchwizard
+        width = float(touchwizard.canvas_width)
+        return width, width
+    
+    def do_get_preferred_height(self, for_width):
+        import touchwizard
+        height = float(touchwizard.canvas_height)
+        return height, height
+    
+    def do_allocate(self, box, flags):
+        canvas_width = box.x2 - box.x1
+        canvas_height = box.y2 - box.y1
+        
+        box = clutter.ActorBox()
+        box.x1 = 0
+        box.y1 = 0
+        box.x2 = canvas_width
+        box.y2 = self.infobar_height
+        self.infobar.allocate(box, flags)
+        
+        box = clutter.ActorBox()
+        box.x1 = 0
+        box.y1 = canvas_height - self.iconbar_height
+        box.x2 = canvas_width
+        box.y2 = canvas_height
+        self.infobar.allocate(box, flags)
+        
+        if self.current_page is not None:
+            box = clutter.ActorBox()
+            box.x1 = 0
+            box.y1 = self.infobar_height
+            box.x2 = canvas_width
+            box.y2 = canvas_height - self.iconbar_height
+            self.current_page.panel.allocate(box, flags)
+    
+    def do_foreach(self, func, data=None):
+        children = [self.infobar, self.iconbar]
+        if self.current_page is not None:
+            children.append(self.current_page.panel)
+        for child in children:
+            func(child, data)
+    
+    def do_paint(self):
+        children = [self.infobar, self.iconbar]
+        if self.current_page is not None:
+            children.append(self.current_page.panel)
+        for child in children:
+            child.paint()
+
+
+def quick_launch(page):
+    import touchwizard
+    stage = clutter.Stage()
+    stage.set_size(touchwizard.canvas_width, touchwizard.canvas_height)
+    if page is not None:
+        stage.set_title(page.title)
+    stage.connect('destroy', clutter.main_quit)
+    
+    canvas = Canvas(page)
+    
+    stage.add(canvas)
+    stage.show()
+
+    clutter.main()
+
+
+if __name__ == '__main__':
+    quick_launch(None)
