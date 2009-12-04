53e46de73da2d78ee6c19911f71e3bf5082df2b5
Damien Boucard <damien.boucard@ubicast.eu>
2009-12-04 13:07:48 +0100

    Adding icon import and global setting to __init__

--- touchwizard/__init__.py
+++ touchwizard/__init__.py
@@ -1,8 +1,9 @@
 from version import VERSION as __revision__
 from canvas import Canvas, quick_launch
 from page import Page
-#from icon import Icon
+from icon import Icon
 
 page_path = None
+icon_path = None
 canvas_width = 800
 canvas_height = 480
