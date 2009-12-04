67c0e45d4b641dc088a305d4a5d05220833f28e9
Damien Boucard <damien.boucard@ubicast.eu>
2009-12-04 13:05:47 +0100

    Adding icon widget ; Adding script to test icon features ;

---
+++ tests/test_icon.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import clutter
+import touchwizard
+import easyevent
+import sys
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    stream=sys.stderr)
+
+stage = clutter.Stage()
+stage.set_size(640, 480)
+stage.connect('destroy', clutter.main_quit)
+
+green = touchwizard.Icon('green_button')
+green.build()
+
+blue = touchwizard.Icon('blue_button')
+blue.build()
+blue.set_x(132)
+
+red = touchwizard.Icon('red_button')
+red.build()
+red.set_x(264)
+
+easyevent.forward_event(
+                     'icon_red_button_actioned', 'lock_icon_blue_button', True)
+easyevent.forward_event(
+                  'icon_green_button_actioned', 'lock_icon_blue_button', False)
+
+stage.add(green, blue, red)
+stage.show()
+
+clutter.main()
---
+++ touchwizard/icon.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+
+import os
+import clutter
+import gobject
+import easyevent
+import candies2
+import logging
+
+logger = logging.getLogger('icon')
+
+class IconRef(object):
+    
+    def __init__(self, icon, label=None, is_locked=None):
+        self.icon = icon
+        self.label = label
+        self.is_locked = is_locked
+    
+    def get_icon(self):
+        icon = Icon(self.icon.name, self.icon.label)
+        if self.label is not None:
+            icon.label = self.label
+        if self.is_locked is not None:
+            icon.is_locked = self.is_locked
+        return icon
+
+
+class Icon(clutter.Actor, clutter.Container, easyevent.User):
+    ACTION_ANIMATE_AND_OPERATE, ACTION_ANIMATE_ONLY, ACTION_OPERATE_ONLY = \
+                                                                       range(3)
+    __gtype_name__ = 'Icon'
+    default_cooldown = 100
+    actioned_event_type_pattern = 'icon_%s_actioned'
+    lock_event_type_pattern = 'lock_icon_%s'
+    action_event_type_pattern = 'action_icon_%s'
+    
+    def __init__(self, name, label=None):
+        self.name = name
+        if label is None:
+            label = name.replace('_', ' ').title()
+        self.label_text = label
+        self.event_type = self.actioned_event_type_pattern %(name)
+        self.cooldown_ms = self.default_cooldown
+        self.is_locked = False
+    
+    def build(self):
+        clutter.Actor.__init__(self)
+        easyevent.User.__init__(self)
+        
+        # Remotely (un)lock icon
+        lock_event_type = self.lock_event_type_pattern %(self.name)
+        #logger.debug('Registering to event type %s.', lock_event_type)
+        self.register_event(lock_event_type)
+        setattr(self, 'evt_' + lock_event_type, self.evt_lock_icon)
+        
+        # Remotely simulate a button click (operation and/or animation)
+        action_event_type = self.action_event_type_pattern %(self.name)
+        #logger.debug('Registering to event type %s.', action_event_type)
+        self.register_event(action_event_type)
+        setattr(self, 'evt_' + action_event_type, self.action)
+        
+        import __init__ as touchwizard
+        picture_path = \
+               os.path.join(touchwizard.icon_path or '', "%s.png" %(self.name))
+        if not os.path.exists(picture_path):
+            self.picture = candies2.ClassicButton('no icon')
+        else:
+            self.picture = clutter.Texture(picture_path)
+            self.picture.set_keep_aspect_ratio(True)
+        candies2.SimpleClick(self.picture)
+        self.picture.set_parent(self)
+        self.picture.set_reactive(True)
+        self.picture.connect('simple-click-event',
+                                                     lambda src: self.action())
+        
+        self.timeline = clutter.Timeline(600)
+        alpha = clutter.Alpha(self.timeline, clutter.EASE_OUT_ELASTIC)
+        self.animation = \
+                        clutter.BehaviourScale(1.1, 1.1, 1.0, 1.0, alpha=alpha)
+        self.animation.apply(self.picture)
+        
+        self.label = clutter.Text()
+        self.label.set_parent(self)
+        self.label.set_text(self.label_text)
+    
+    """
+    def do_get_preferred_width(self, for_height):
+        pass
+    
+    def do_get_preferred_height(self, for_width):
+        pass
+    """
+    
+    def do_allocate(self, box, flags):
+        boxes = {
+            self.label: clutter.ActorBox(),
+            self.picture: clutter.ActorBox()
+        }
+        label_width = self.label.get_preferred_size()[2]
+        label_height = self.label.get_preferred_size()[3]
+        picture_width = self.picture.get_preferred_size()[2]
+        picture_height = self.picture.get_preferred_size()[3]
+        
+        if label_width > picture_width:
+            largest = self.label
+            thinest = self.picture
+        else:
+            thinest = self.label
+            largest = self.picture
+        largest_width = largest.get_preferred_size()[2]
+        thinest_width = thinest.get_preferred_size()[2]
+        boxes[thinest].x1 = (largest_width - thinest_width) / 2
+        boxes[largest].x1 = 0
+        
+        boxes[self.label].x2 = boxes[self.label].x1 + label_width
+        boxes[self.label].y1 = 0
+        boxes[self.label].y2 = label_height
+        
+        boxes[self.picture].x2 = boxes[self.picture].x1 + picture_width
+        boxes[self.picture].y1 = boxes[self.label].y2
+        boxes[self.picture].y2 = boxes[self.picture].y1 + picture_height
+        
+        self.label.allocate(boxes[self.label], flags)
+        self.picture.allocate(boxes[self.picture], flags)
+        clutter.Actor.do_allocate(self, box, flags)
+    
+    def action(self, event=None):
+        what_to_do = self.ACTION_ANIMATE_AND_OPERATE
+        if event is not None and event.content is not None:
+            what_to_do = event.content
+        if what_to_do in (self.ACTION_ANIMATE_AND_OPERATE, self.ACTION_OPERATE_ONLY):
+            if not self.is_locked:
+                self.lock_for(self.cooldown_ms)
+                self.launch_event(self.event_type)
+            else:
+                self.launch_event('info',
+                               'Please wait %d seconds' %(self.cooldown_ms / 1000))
+        if what_to_do in (self.ACTION_ANIMATE_AND_OPERATE, self.ACTION_ANIMATE_ONLY):
+            self.animate()
+    
+    def animate(self):
+        self.timeline.start()
+    
+    def lock(self):
+        logger.debug('Locking %s.', self.name)
+        self.is_locked = True
+        self.picture.set_opacity(80)
+        return True
+
+    def lock_for(self, duration):
+        self.lock()
+        gobject.timeout_add(duration, self.unlock)
+
+    def unlock(self):
+        logger.debug('Unlocking %s.', self.name)
+        self.is_locked = False
+        self.picture.set_opacity(255)
+        return False
+
+    def evt_lock_icon(self, event):
+        if event.content is not False:
+            operation = 'lock'
+        else:
+            operation = 'unlock'
+        logger.debug('Remote %s request received for %s.',
+                                                          operation, self.name)
+        call_method = getattr(self, operation)
+        call_method()
+    
+    def do_foreach(self, func, data=None):
+        children = (self.label, self.picture)
+        for child in children:
+            func(child, data)
+    
+    def do_paint(self):
+        children = (self.label, self.picture)
+        for child in children:
+            child.paint()
+    
+    def do_pick(self, color):
+        self.do_paint()
